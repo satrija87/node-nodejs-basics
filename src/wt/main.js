9b80767f81ef54b2ff4b80e6b0a620a3ab8de4fd
satrija87 <satrija87@gmail.com>
2025-10-26 19:37:13 +0100

    feat: add solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import path from 'path';
+
+const __dirname = path.dirname(import.meta.filename);
+const workerPath = path.join(__dirname, 'worker.js');
+const countStart = 10;
+const countCPUs = cpus().length;
+
 const performCalculations = async () => {
-  // Write your code here
+  const workerPromises = Array.from({ length: countCPUs }, (_, i) => {
+    const worker = new Worker(workerPath, { workerData: countStart + i });
+
+    return new Promise((resolve) => {
+      worker.on('message', (data) => resolve(data));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+      worker.on('exit', (code) => {
+        if (code !== 0) resolve({ status: 'error', data: null });
+      });
+    });
+  });
+
+  const results = await Promise.all(workerPromises);
+  console.log(results);
 };
 
 await performCalculations();
